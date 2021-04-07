f2bbe39faf68c2e787d16b31beb0f44ee080a103
Jonas Sousa <jonas.academico1@gmail.com>
2021-04-07 18:09:01 -0300

    Correção no spawn de inimigos.

--- main.py
+++ main.py
@@ -314,8 +314,8 @@ def main():
         TELA.blit(BG, (0, 0))
 
         #Spawn aleatório de inimigos
-        if len(naves)<6:
-            inimigotempo = Inimigo(random.randint(0, 500), 0)
+        if len(naves) < 6:
+            inimigotempo = Inimigo(random.randint(0, 500), 0,random.randint(0,2))
             naves.append(inimigotempo)
 
         for event in pygame.event.get():
